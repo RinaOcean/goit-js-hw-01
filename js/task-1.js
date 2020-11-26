22216f17a31739910c8c47a651cde937f585307b
RinaOcean <antyfifa@gmail.com>
2020-11-26 22:24:51 +0200

    task-1

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+"use strict"
+
+let price = 1000
+const name = "Генератор защитного поля"
+
+console.log(`Выбран «${name}», цена за штуку ${price} кредитов`)
+
+price = 2000
+
+console.log(`Выбран «${name}», цена за штуку ${price} кредитов`)
