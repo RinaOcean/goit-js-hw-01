8f7300f5138533056199115f922712c47ceed989
RinaOcean <antyfifa@gmail.com>
2020-11-27 00:19:20 +0200

    task-3

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const ADMIN_PASSWORD = "jqueryismyjam";
+let message;
+const login = prompt("Пожалуйста, введите пароль");
+
+if (login === ADMIN_PASSWORD) {
+  message = "Добро пожаловать!";
+} else if (login === null) {
+  message = "Отменено пользователем!";
+} else {
+  message = "Доступ запрещен, неверный пароль!";
+}
+
+alert(message);
